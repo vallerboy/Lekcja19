6a2e7f6097255d68d5308638a683f4c56a6ebc3f
vallerboy <oskarbank@gmail.com>
2016-09-26 18:39:20 +0200

    First vibrator commit

--- app/src/main/java/pl/oskarpolak/lekcja13/MainActivity.java
+++ app/src/main/java/pl/oskarpolak/lekcja13/MainActivity.java
@@ -3,10 +3,12 @@ package pl.oskarpolak.lekcja13;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.SystemClock;
+import android.os.Vibrator;
 import android.support.v4.app.Fragment;
 import android.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -32,6 +34,21 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+       // vibrator.vibrate(1000);
+
+        if(vibrator.hasVibrator()){
+            Log.e("Vibrator", "Może wibrować");
+        }else{
+            Log.e("Vibrator", "Nie ma opcji wibrowania");
+        }
+
+        // 0 - przerwa, 1 - długość, 2 - przerwa, 3 - długość, 4 - przerwa, 5 - długość [..]
+        long[] pattern = {0, 500, 100, 500, 100, 500, 100, 500};
+        vibrator.vibrate(pattern, -1);
+
+        // Użyj aby anulować wibracje
+        // vibrator.cancel();
 
 
     }
